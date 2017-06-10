0541e4720cef60c2c8284a4a79816f3052b2b453
Aman Kubanychbek <amankubanychbek@gmail.com>
2017-06-10 22:11:28 +0600

    remove iife output, only cjs/es

--- rollup.config.js
+++ rollup.config.js
@@ -4,14 +4,18 @@ import eslint from 'rollup-plugin-eslint'
 import resolve from 'rollup-plugin-node-resolve'
 import uglify from 'rollup-plugin-uglify'
 
-const config = {
+const isProd = process.env.NODE_ENV === 'production'
+
+export default {
   entry: 'src/index.js',
   dest: 'dist/index.js',
+  format: isProd ? 'cjs' : 'es',
   plugins: [
     resolve(),
     commonjs(),
     eslint(),
     babel({ exclude: 'node_modules/**' }),
+    isProd && uglify(),
   ],
   external: [
     'prop-types',
@@ -22,12 +26,3 @@ const config = {
   ],
   sourceMap: true,
 }
-if (process.env.NODE_ENV === 'production') {
-  config.format = 'cjs'
-  config.plugins.push(uglify())
-} else {
-  config.format = 'iife'
-  config.moduleName = 'main'
-}
-
-export default config
