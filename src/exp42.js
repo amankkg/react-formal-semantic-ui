03328adf18d06bd05ce7a787c48a1d7cfb3af6f6
Aman Kubanychbek <amankubanychbek@gmail.com>
2017-07-01 23:18:28 +0600

    WIP add flowtype and babel (remove rollup entirely?)

---
+++ src/exp42.js
@@ -0,0 +1,5 @@
+// @flow
+
+const exp42: number = (x: number) => x ** 42
+
+export default exp42
--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-export default x => x ** 42
+// @flow
+import exp42 from './exp42'
+
+class Person {
+  constructor(age: number) {
+    this.age = age
+  }
+
+  getAgeInExp = () => exp42('24');
+}
+
+export default Person
